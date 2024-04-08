516f40d7cd96b8594ecef52e99a749e0985d1449
Sabli Junior Abakar Arbab <sabakar-@bess-f1r1s11.clusters.42paris.fr>
2024-04-08 11:26:23 +0200

    Adding split

--- src/pipex.c
+++ src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: sabakar- <sabakar-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/05 11:38:42 by sabakar-          #+#    #+#             */
-/*   Updated: 2024/04/05 19:16:37 by sabakar-         ###   ########.fr       */
+/*   Updated: 2024/04/05 22:24:41 by sabakar-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,9 +65,10 @@ void	*ft_first_child_process(t_pipex *data, char **args, char **env)
 		close(data->in_file);
 		return (ft_print_err("An Error occoured with dup2"), NULL);
 	}
+	close(data->fd[1]);
 	cmd = ft_split(args[1], ' ');
 	la_path = ft_check_path(cmd[0], env);
-	close(data->fd[0]);
+	close(data->in_file);
 	execev(la_path, cmd, env);
 	ft_print_err("An error has occured on execev");
 	return (free(cmd), free(la_path), NULL);
@@ -79,7 +80,7 @@ void	*ft_second_child_process(t_pipex *data, char **args, char **env)
 	char	*la_path;
 
 	close(data->fd[1]);
-	data->out_file = open(args[4], O_RDONLY | O_WRONLY | O_CREAT | 0777);
+	data->out_file = open(args[4], O_CREAT | O_RDONLY | O_WRONLY, 0777);
 	if (data->out_file < 0)
 		return (close(data->fd[0]), ft_print_err("Error opening file"), NULL);
 	if (dup2(data->out_file, 1) == -1)
@@ -90,9 +91,10 @@ void	*ft_second_child_process(t_pipex *data, char **args, char **env)
 		close(data->fd[0]);
 		return (ft_print_err("An err occoured with dup2"), NULL);
 	}
+	close(data->fd[0]);
 	cmd = ft_split(args[3], ' ');
 	la_path = ft_cheak_path(cmd[0], env);
-	close(data->fd[1]);
+	close(data->out_file);
 	execve(la_path, cmd, env);
 	return (free(cmd), free(la_path), NULL);
 }
